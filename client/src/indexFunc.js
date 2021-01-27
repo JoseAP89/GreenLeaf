11cccad5c7fc0275d75e8c753ea644c866049c06
Jose APumarino <josxe89@gmail.com>
2021-01-26 22:23:22 -0600

    first commit

---
+++ client/src/indexFunc.js
@@ -0,0 +1,5 @@
+console.log("HERE!!");
+var dateControl = document.querySelector('input[type="date"]');
+dateControl.value = '2017-06-01';
+console.log(dateControl.value); 
+console.log(dateControl.valueAsNumber);
